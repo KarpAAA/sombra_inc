612f4f4c12abedc9f50f06efbbdb68f00d65321e
KarpAAA <ivankarpinskij.ua@gmail.com>
2023-12-11 20:55:19 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+
+  <div class="main">
+    <div class="charactersContainer">
+      <div v-for="c in characters" class="character">
+        <character_component :character="c"></character_component>
+      </div>
+    </div>
+  </div>
+
+
+</template>
+
+<script>
+import Character_component from "@/components/CharacterComponent.vue";
+import ivan from "@/assets/ivan.jpg";
+export default {
+  name: "app",
+  components: {Character_component},
+  data(){
+    return {
+      characters: [
+        
+      ]
+    }
+  },
+}
+</script>
+<style scoped>
+.character{
+  width: 47%;
+  margin: 15px
+}
+.charactersContainer {
+  margin-top: 65px;
+  width: 75%;
+  display: flex;
+  flex-wrap: wrap;
+}
+.main {
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ src/components/CharacterComponent.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="character1">
+    <div>
+      <div :style='{backgroundImage:`url(${this.character.image})`, width: "200px", height: "200px", borderRadius: "15px" }'>
+      </div>
+    </div>
+
+    <div class="info">
+      <div>Position: {{ this.character.role }}</div>
+      <div>Name: {{ this.character.name }}</div>
+      <div>Salary: {{ this.character.salary }} $</div>
+    </div>
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "character_component",
+  data() {
+    return {
+      some: String
+    }
+  },
+  props: {
+    character: {
+      name: String,
+      role: String,
+      image: String,
+      salary: Number,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.info{
+  padding-left: 30px;
+  margin-top: 14%;
+  font-size: 18px;
+}
+.character1 {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 30px;
+  border-radius: 15px;
+  border: 1px solid grey;
+  color: black;
+  font-size: 20px;
+  background-color: white;
+}
+</style>
